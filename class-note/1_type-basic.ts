10e3be3176e9012496a6b2fa5b4ccab9645fd4fc
김동훈A <agmon5959@douzone.com>
2022-06-22 21:03:28 +0900

    commit

---
+++ class-note/1_type-basic.ts
@@ -0,0 +1,7 @@
+// js 문자열 선언
+var str_js = "hello";
+// ts 문자열 선언
+let str_ts: string = "hello";
+let num_ts: number = 123;
+let numArr_ts: Array<Number> = [1, 2, 3];
+let strArr_ts: Array<String> = ['1', '2', '3'];
--- getting-started/index.ts
+++
@@ -1,9 +0,0 @@
-function sum(a: number, b: number): number {
-    return a + b;
-}
-sum(10, 20)
-
-function commitFunc=(): boolean => {
-    let bool = true;
-    return bool;
-}
